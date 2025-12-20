d7d4612392dd522c6211e46d95628ed53fc3a0ae
div-html <pariyardivyam67@gmail.com>
2025-12-20 10:09:03 +0530

    Updated code

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ let section1=document.querySelector(".content2");
 console.log(audio + btn);
 btn.addEventListener("click",function(){
     audio.play();
+    audio.volume=0.5;
     section.classList.add("fadeOut");
-    section1.style.display="block";
+    section.style.display="none";
+    section1.style.display="flex";
     section1.classList.add("fadeInAnimation");
+    section1.classList.add("content2");
 })  ;
