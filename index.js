5e20e1849cb0526f58a25301d2acad96ffe32a49
div-html <pariyardivyam67@gmail.com>
2025-12-22 15:21:28 +0530

    Finished product

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let btn=document.querySelector("#btn1");
 let section=document.querySelector(".content");
 let section1=document.querySelector(".content2");
 let section2=document.querySelector(".content3");
+let section3=document.querySelector(".content4");
 btn.addEventListener("click",function(){
       audio.play();
     audio.volume = 0.5;
@@ -27,3 +28,13 @@ button.addEventListener("click",function(){
     }, 1500);
     
 });
+let button2=document.querySelector("#ext");
+button2.addEventListener("click", function(){
+    section2.classList.remove("fadeInAnimation");
+    section2.classList.add("fadeOutAnimation");
+    setTimeout(()=>{
+        section2.style.display="none";
+        section3.style.display="flex";
+        section3.classList.add("fadeInAnimation");
+    },1500)
+})
